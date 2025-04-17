e4f0f106842155aac5bf416f715c276800681d61
Varshini S <146536557+varshini-s-p@users.noreply.github.com>
2025-04-18 01:18:22 +0530

    Add files via upload
    PhonePe Transaction Insights is a data analytics and visualization project that explores the digital payment landscape across India using PhonePe’s publicly available transaction data. The goal is to uncover trends, regional patterns, user behavior, and business opportunities through advanced SQL queries, Python analysis, interactive dashboards (Streamlit), and compelling visualizations (Power BI).
    This project includes:
    
    End-to-end data extraction and transformation using Python (Colab & Jupyter)
    
    SQL-based insights for five business use cases
    
    Real-time interactive dashboard using Streamlit
    
    Comprehensive dashboards in Power BI highlighting trends and KPIs
    
    Strategic recommendations based on transaction behaviors and regional trends

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="PhonePe Insights", layout="wide")
+
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Home", "Business Case Study"])
+
+if page == "Home":
+    from home import home_page
+    home_page()
+
+elif page == "Business Case Study":
+    from case_study import case_study_page
+    case_study_page()
+
+    
